89245badb70de39dffc0f19fe8a9515f89b3b9bd
Aayush <aayush10738@gmail.com>
2026-03-11 01:57:26 +0530

    Uncomment registration status and reason for Cricket and Volleyball

--- src/lib/sportsConfig.js
+++ src/lib/sportsConfig.js
@@ -32,8 +32,8 @@ export const SPORTS_CONFIG = {
   },
   Cricket: {
     type: "Team Sport",
-    //registrationClosed: true,
-    //registrationClosedReason: "Registration for Cricket is currently paused.",
+    registrationClosed: true,
+    registrationClosedReason: "Registration for Cricket is currently paused.",
     categories: [
       {
         id: "men",
@@ -95,8 +95,8 @@ export const SPORTS_CONFIG = {
   },
   Volleyball: {
     type: "Team Sport",
-    //registrationClosed: true,
-    //registrationClosedReason: "Registration for Volleyball is currently paused.",
+    registrationClosed: true,
+    registrationClosedReason: "Registration for Volleyball is currently paused.",
     categories: [
       {
         id: "men",
